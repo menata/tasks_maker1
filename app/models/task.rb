1198a5e574db676adfd1f8f24c5f38da453a4f9b
nata <nlevchenko@ubuntu.(none)>
2012-12-28 02:24:36 -0800

    assets fixed on pteroduction

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ActiveRecord::Base
   belongs_to :pitem
   attr_accessible :tname, :priority, :expires_at, :pitem_id, :status
-  validates :tname, :priority, presence: true
+  validates :tname, :priority, :expires_at, presence: true
 end
