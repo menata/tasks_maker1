ecfb86f22c27b133193a9086ade1cee54d8df450
nata <nlevchenko@ubuntu.(none)>
2012-12-28 15:28:26 -0800

    assets3

---
+++ app/assets/javascripts/table.js
@@ -0,0 +1,26 @@
+   $(document).ready(function(){
+        $("#report tr:odd").addClass("odd");
+        $("#report tr:not(.odd)").hide();
+        $("#report tr:first-child").show();
+
+        $("#report tr.odd").mouseover(function(){
+            $(this).next("tr").toggle();
+            $(this).find(".arrow").toggleClass("up"); 
+        });
+
+        $("#report tr:not(.odd)").mouseover(function(){ 
+            $(this).show();
+        });
+
+        $(".span2").click(function(){
+            $(this).val(''); 
+        });
+
+        $("#report").mouseout(function(){
+            $("#report tr:not(.odd)").hide();
+        });
+            //$("#report").jExpand();
+        });
+
+
+
