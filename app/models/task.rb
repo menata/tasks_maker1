238dc3734657c997de1082384c8385768d281f34
nata <nlevchenko@ubuntu.(none)>
2012-12-27 18:40:51 -0800

    table up

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ActiveRecord::Base
   belongs_to :pitem
   attr_accessible :tname, :priority, :expires_at, :pitem_id, :status
-  validates :tname, :priority, :expires_at, presence: true
+  validates :tname, :priority, presence: true
 end
