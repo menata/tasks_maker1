ce13cd720f801109f0c76256089a8f9b5663e075
nata <nlevchenko@ubuntu.(none)>
2012-12-30 00:27:14 -0800

    table css

--- app/assets/javascripts/table.js
+++ app/assets/javascripts/table.js
@@ -1,3 +1,23 @@
-  
+   $(document).ready(function(){
+        $("#report tr:odd").addClass("odd");
+        $("#report tr:not(.odd)").hide();
+        $("#report tr:first-child").show();
 
+        $("#report tr.odd").mouseover(function(){
+            $(this).next("tr").toggle();
+            $(this).find(".arrow").toggleClass("up"); 
+        });
 
+        $("#report tr:not(.odd)").mouseover(function(){ 
+            $(this).show();
+        });
+
+        $(".span2").click(function(){
+            $(this).val(''); 
+        });
+
+        $("#report").mouseout(function(){
+            $("#report tr:not(.odd)").hide();
+        });
+            //$("#report").jExpand();
+        });
