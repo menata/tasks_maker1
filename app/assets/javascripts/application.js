e095fdf9a4b930932e52c7457303a1331785f7a8
nata <nlevchenko@ubuntu.(none)>
2013-01-02 19:53:34 -0800

    table css16

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,12 @@
 //= require jquery-ui
 // require jquery_ujs
 //= require table
+
+function check()
+   {  
+     if (ff.value() == "") then
+    { 
+      alert("Type text");
+    }
+      else{}
+   }
