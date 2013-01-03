0b80cfb2eafe4b162a92c8b871f02e1a251dd00d
nata <nlevchenko@ubuntu.(none)>
2013-01-02 20:07:22 -0800

    Revert "table css16"
    This reverts commit e095fdf9a4b930932e52c7457303a1331785f7a8.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,12 +2,3 @@
 //= require jquery-ui
 // require jquery_ujs
 //= require table
-
-function check()
-   {  
-     if (ff.value() == "") then
-    { 
-      alert("Type text");
-    }
-      else{}
-   }
