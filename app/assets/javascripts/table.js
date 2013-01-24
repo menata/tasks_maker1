392b59f35a23b14b08326eded5868edce0c9b03c
nata <nlevchenko@ubuntu.(none)>
2013-01-23 19:48:49 -0800

    --

--- app/assets/javascripts/table.js
+++ app/assets/javascripts/table.js
@@ -1,10 +1,10 @@
-        $(document).ready(function(){
+         $(document).ready(function(){
           $('div[id*="p"]').each(function(index, el) {
               animatedcollapse.addDiv(el.id);            
             }); 
-
+          //dock project name row
          $("#report a#cl").click(function()
-        {
+          {
           if(!$(this).closest("tr").next("tr").hasClass("zzz"))
           {
              $(this).closest("tr").next("tr").addClass("zzz");
@@ -17,22 +17,24 @@
              $(this).closest("tr").next("tr").removeClass("zzz");                           
           }
         });
+        //show-hide rows
         $("#report tr:odd").addClass("odd");
         $("#report tr:not(.odd)").hide();        
         $("#report tr:first-child").show();
         $("#report tr.odd").mouseover(function(){
-            if(!$(this).next("tr").hasClass("zzz"))
+        //leave docket row visible     
+        if(!$(this).next("tr").hasClass("zzz"))
             {
               $(this).next("tr").toggle();
               $(this).find(".arr").toggleClass("up");
             }  
-             
+              
         });
-        $("#report tr.odd").mouseout(function(){ 
+        $("#report tr.odd").click(function(){ 
         $(this).next('tr').find(".l").each(function(index,el){
                animatedcollapse.hide(el.id);
                }); 
-          });   
+          }); 
         $("#report tr:not(.odd)").mouseover(function(){ 
              $(this).show();
              $(this).find(".l").each(function(index,el){
@@ -48,10 +50,11 @@
             $("#report tr.zzz").show();           
         });
         $("#report div.l").mouseover(function(){
-            $(this).css('background','#ffffcc');
+            $(this).css('background','#FFD699');
+        
         });
         $("#report div.l").mouseout(function(){
-            $(this).css('background','#ffffff');
+            $(this).css('background','');
         });
         $("#report tr#1").css('display','none');   
                                      
